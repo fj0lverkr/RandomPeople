8034466da22841cd9d43794a6ac1dd98f2f80ccd
Nils Nahooy <nils.nahooy@live.com>
2023-12-11 12:27:14 +0100

    add animation on hover

--- src/components/PeopleTile.tsx
+++ src/components/PeopleTile.tsx
@@ -1,10 +1,26 @@
+import { MouseEvent } from "react";
 interface Props {
   personObject: any;
 }
 const PeopleTile = ({ personObject }: Props) => {
+  const handleMouseHoverEvent = (e: MouseEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    if (e.type === "mouseenter") {
+      e.currentTarget.classList.remove("shadow-sm");
+      e.currentTarget.classList.add("shadow");
+    } else {
+      e.currentTarget.classList.remove("shadow");
+      e.currentTarget.classList.add("shadow-sm");
+    }
+  };
   return (
     <>
-      <div className="card shadow-sm">
+      <div
+        role="button"
+        className="card shadow-sm"
+        onMouseEnter={handleMouseHoverEvent}
+        onMouseLeave={handleMouseHoverEvent}
+      >
         <div className="card-body">
           <img
             className="rounded-circle shadow-4-strong mx-auto d-block"
